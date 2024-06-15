35a12476c7bd0473a906d8cc2baef7ad8665d0ff
LeafusBonesboro <108142251+LeafusBonesboro@users.noreply.github.com>
2024-06-14 23:08:09 -0400

    added profile display for api and profiles

--- app/api/profile.py
+++ app/api/profile.py
@@ -1,6 +1,9 @@
+import json
+
 from flask_restful import Resource
 
+with open("profiles.json", "r") as f:
+    db = json.load(f)
 class ProfileResource(Resource):
     def get(self):
-        return {"profile": "resource"}
-    
+        return sorted(db, key=lambda x: x["last_name"])
